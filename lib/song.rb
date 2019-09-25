c4ff6b032210cf848361da1309a05aede2bc697e
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-25 17:44:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,12 @@
 ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
-
+@@count = count
+@@count.initialize
 ninety_nine_problems.name
+
+
+ninety_nine_problems.artist
+
+
+ninety_nine_problems.genre
+@@genres = genres
+@@genres.initialize
